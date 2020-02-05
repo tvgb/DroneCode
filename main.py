dbe3d9dd29451714011ba7df7d278099caea3557
Gjelseth-Borgen Trym Vegard <Trym.Vegard.Gjelseth-Borgen@norconsult.com>
2020-02-05 14:26:10 +0100

    Added drone.connection()

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from controller import get_drone, set_gimbal
 
 print('Creating drone')
 drone = get_drone('192.168.42.1')
+drone.connection()
 print('Drone created \n')
 
 print('Setting gimbal')
-set_gimbal(drone, 45, 45, 45)
+set_gimbal(drone, 45.0, 45.0, 45.0)
 print('Gimbal set')
