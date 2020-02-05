eb6bc709b4e722615090fd71b88ae3addff3c7f9
Fredrik H <fredrikiho@hotmail.com>
2020-02-05 14:17:55 +0100

    Should work now

---
+++ controller.py
@@ -0,0 +1,50 @@
+# -*- coding: UTF-8 -*-
+
+import olympe
+from olympe.messages import gimbal
+from olympe.messages.ardrone3.Piloting import TakeOff, Landing
+
+def get_drone(ip):
+    drone = olympe.Drone(ip, loglevel=4)
+    print('Created drone')
+    return drone
+
+def set_gimbal(drone,set_yaw,set_pitch,set_roll):
+    drone(gimbal.set_target(
+        gimbal_id=0,
+        control_mode="position",
+        yaw_frame_of_reference="none",
+        yaw=set_yaw,
+        pitch_frame_of_reference="absolute",
+        pitch=set_pitch,
+        roll_frame_of_reference="none",
+        roll=set_roll,
+    )).wait()
+
+
+
+
+
+
+'''
+
+drone.connection()
+drone(TakeOff()).wait()
+drone(Landing()).wait()
+drone.disconnection()
+
+
+
+def camera_straight(drone):
+    drone(gimbal.set_target(
+        gimbal_id=0,
+        control_mode="position",
+        yaw_frame_of_reference="none",
+        yaw=0.0,
+        pitch_frame_of_reference="absolute",
+        pitch=0.0,
+        roll_frame_of_reference="none",
+        roll=0.0,
+    )).wait() 
+
+'''
