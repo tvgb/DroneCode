3d630531690e83b12454836405024b8261d4a6da
Fredrik H <fredrikiho@hotmail.com>
2020-02-12 14:56:17 +0100

    lolol

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from controller import get_drone, set_gimbal
 import time
-
+import com.parrot.drone.groundsdk.device.instrument as instrument
 
 print('Creating drone')
 drone = get_drone('192.168.42.1')
 drone.connection()
 print('Drone created \n')
+
+print(instrument.lastKnownLocation())
 
 print('Setting gimbal')
 set_gimbal(drone, "position", 0.0, 0.0, 0.0)
