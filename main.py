86d8371ad23d05eae2839dc490a064bf965dc6a5
Fredrik H <fredrikiho@hotmail.com>
2020-02-12 15:02:28 +0100

    lolol

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from controller import get_drone, set_gimbal
 import time
-import com.parrot.drone.groundsdk.device.instrument as instrument
+from __future__ import print_function
+from olympe.messages.ardrone3.PilotingState import PositionChanged
+from olympe.messages.ardrone3.GPSSettingsState import GPSFixStateChanged
+from olympe.messages.ardrone3.Piloting import TakeOff
+from olympe.messages.ardrone3.GPSSettingsState import HomeChanged
 
 print('Creating drone')
 drone = get_drone('192.168.42.1')
 drone.connection()
 print('Drone created \n')
 
-print(instrument.lastKnownLocation())
+drone(GPSFixStateChanged(_policy = 'wait'))
+
+print("GPS position before take-off :", drone.get_state(HomeChanged))
 
 print('Setting gimbal')
 set_gimbal(drone, "position", 0.0, 0.0, 0.0)
