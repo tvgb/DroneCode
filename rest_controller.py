05f451aeacd0a204d48907d7d47b61bc41897515
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2020-02-19 10:41:22 +0100

    Created a new REST endpoint

--- rest_controller.py
+++ rest_controller.py
@@ -1,8 +1,19 @@
 from flask import Flask
+import controller
 
 app = Flask(__name__)
+
+drone = None # Global drone vriable
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+
+@app.route('/connectToDrone')
+def connect_to_drone():
+    global drone
+    drone = controller.get_drone('192.168.42.1')
+    drone.connection()
+    return 'Connecting to drone'
 
