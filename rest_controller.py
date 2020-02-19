093e02d34d840dd62d293bfa0774491b18f1f71f
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2020-02-19 14:37:44 +0100

    Created routes for moving

--- rest_controller.py
+++ rest_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import controller
 
 app = Flask(__name__)
@@ -27,7 +27,24 @@ def take_off():
     controller.takeoff(drone)
     return 'taking off'
 
+@app.route('/moveTo/<float:latitude>/<float:longitude>/<float:altitude>')
+def move_to(latitude, longitude, altitude):
+    controller.moveto(drone, latitude, longitude, altitude)
+    return jsonify({
+        'message': f'Flying to lat: {latitude}, long: {longitude}, alt: {altitude}'
+    }), 200
+
+@app.route('/moveBy/<float:x>/<float:y>/<float:z>')
+def move_by(x, y, z):
+    controller.move(drone, x, y, z)
+    return jsonify({
+        'message': f'Flying {x} in x direction, {y} in y direction, {z} in z direction'
+    }), 200
+
 @app.route('/land')
 def land():
     controller.land(drone)
     return 'landing'
+
+@app.route('/disconnect')
+def disconnect():
