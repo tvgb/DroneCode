ed1d83e6c9d853b5e0af55635b5e6789aa9e50ec
Fredrik H <fredrikiho@hotmail.com>
2020-02-19 14:39:12 +0100

    lolol

--- rest_controller.py
+++ rest_controller.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import controller
+from olympe.messages.ardrone3.Piloting import TakeOff, moveBy, Landing, moveTo
 
 app = Flask(__name__)
 
@@ -9,14 +10,13 @@ drone = None # Global drone vriable
 def hello_world():
     return 'Hello world!'
 
-
 @app.route('/connectToDrone')
 def connect_to_drone():
     global drone
     drone = controller.get_drone('192.168.42.1')
     drone.connection()
+    print('gimbal state: ',olympe.enums.gimbal.state)
     return 'Connecting to drone'
-
 
 @app.route('/getPosition')
 def get_position():
@@ -26,6 +26,16 @@ def get_position():
 def take_off():
     controller.takeoff(drone)
     return 'taking off'
+
+@app.route('/flyBack')
+def flyBackwards():
+    drone.moveby(drone,-1,0,0,0)
+    return 'flying 1 meter backwards'
+
+@app.route('/flyForward')
+def flyForwards():
+    drone.moveby(drone,1,0,0,0)
+    return 'flying 1 meter forwards'
 
 @app.route('/land')
 def land():
