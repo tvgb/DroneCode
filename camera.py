aba4134085300385c76d1e2cd93dfa7426ff49c2
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2020-03-11 14:59:14 +0100

    funny

--- camera.py
+++ camera.py
@@ -5,13 +5,12 @@ from base_camera import BaseCamera
 class Camera(BaseCamera):
     """An emulated camera implementation that streams a repeated sequence of
     files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
-    imgs = [open(f'./static/images/mario/mario{f}.jpg', 'rb').read() for f in range(1, 13)]
+    imgs = open('./images/test_image.jpg', 'rb').read()
 
     @staticmethod
     def frames():
 
-        
         while True:
             print('sending image')
-            time.sleep(0.1)
-            yield Camera.imgs[int(time.time()) % 12]
+            time.sleep(1/30)
+            yield Camera.imgs
