fdfaf22bc59df6abaafca66a3d4315bf3b710826
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2020-03-11 10:50:00 +0100

    Some things are working

--- camera.py
+++ camera.py
@@ -1,8 +1,15 @@
-from time import time
+import time
+from base_camera import BaseCamera
 
-class Camera(object):
-    def __init__(self):
-        self.frames = [open(f'./static/images/mario/mario{f}.jpg', 'rb').read() for f in range(1, 13)]
 
-    def get_frame(self):
-        return self.frames[int(time()) % 12]
+class Camera(BaseCamera):
+    """An emulated camera implementation that streams a repeated sequence of
+    files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
+    imgs = [open(f'./static/images/mario/mario{f}.jpg', 'rb').read() for f in range(1, 13)]
+
+    @staticmethod
+    def frames():
+        while True:
+            print('sending image')
+            time.sleep(0.1)
+            yield Camera.imgs[int(time.time()) % 12]
--- rest_controller.py
+++ rest_controller.py
@@ -1,11 +1,18 @@
 from flask import Flask, jsonify, request, render_template, Response
 from flask_cors import CORS, cross_origin
 from camera import Camera
-# import controller
-import sys
+import controller
+import sys, os
 from PIL import Image
-# from olympe.messages.ardrone3.Piloting import Landing
-# from haversine import haversine, Unit
+from importlib import import_module
+
+from olympe.messages.ardrone3.Piloting import Landing
+from haversine import haversine, Unit
+
+if os.environ.get('CAMERA'):
+    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+else:
+    from camera import Camera
 
 app = Flask(__name__)
 CORS(app)
@@ -23,166 +30,167 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+
 @app.route('/video_feed', methods=['GET'])
 def video_feed():
     return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-# @app.route('/connectToDrone', methods=['POST'])
-# def connect_to_drone():
-#     global drone
+@app.route('/connectToDrone', methods=['POST'])
+def connect_to_drone():
+    global drone
 
-#     try:
-#         drone = controller.get_drone('192.168.42.1')
-#         connection = drone.connection()
+    try:
+        drone = controller.get_drone('192.168.42.1')
+        connection = drone.connection()
 
-#         if connection[0]:
-#             return jsonify({
-#                 'message': 'Connected to drone'
-#             }), 200
-#         else:
-#             return jsonify({
-#                 'message': 'Could not connect to drone'
-#             }), 200
+        if connection[0]:
+            return jsonify({
+                'message': 'Connected to drone'
+            }), 200
+        else:
+            return jsonify({
+                'message': 'Could not connect to drone'
+            }), 200
 
-#     except:
-#         print('error')
+    except:
+        print('error')
 
-#         return jsonify({
-#             'message': 'Could not connect to drone',
-#             'error' : sys.exc_info()[0]
-#         }), 500
-
-
-# @app.route('/getPosition', methods=['POST'])
-# def get_position():
-#     try:
-#         position = controller.position(drone)
-#         return jsonify({
-#             'message': f'Drone is at position: {position}'
-#         })
-#     except:
-#         return jsonify({
-#             'message': 'Could not get drone position',
-#             'error' : sys.exc_info()[0]
-#         }), 500
+        return jsonify({
+            'message': 'Could not connect to drone',
+            'error' : sys.exc_info()[0]
+        }), 500
 
 
-# @app.route('/takeOff', methods=['POST'])
-# def take_off():
-#     try:
-#         controller.takeoff(drone)
-#         return jsonify({
-#             'message': 'Drone is taking off'
-#         })
-#     except:
-#         drone(Landing()).wait()
-#         return jsonify({
-#             'message': 'Failed when trying to take off. Landing...',
-#             'error': sys.exc_info()[0]
-#         }), 500
+@app.route('/getPosition', methods=['POST'])
+def get_position():
+    try:
+        position = controller.position(drone)
+        return jsonify({
+            'message': f'Drone is at position: {position}'
+        })
+    except:
+        return jsonify({
+            'message': 'Could not get drone position',
+            'error' : sys.exc_info()[0]
+        }), 500
 
 
-# @app.route('/moveTo', methods=['POST'])
-# def move_to():
+@app.route('/takeOff', methods=['POST'])
+def take_off():
+    try:
+        controller.takeoff(drone)
+        return jsonify({
+            'message': 'Drone is taking off'
+        })
+    except:
+        drone(Landing()).wait()
+        return jsonify({
+            'message': 'Failed when trying to take off. Landing...',
+            'error': sys.exc_info()[0]
+        }), 500
+
+
+@app.route('/moveTo', methods=['POST'])
+def move_to():
     
-#     maxDistanceMoved = 5
+    maxDistanceMoved = 5
 
-#     try:
-#         dronePos = controller.position(drone)
-#         drone_latitude = dronePos['latitude']
-#         drone_longitude = dronePos['longitude']
-#         drone_altitude = dronePos['altitude']
+    try:
+        dronePos = controller.position(drone)
+        drone_latitude = dronePos['latitude']
+        drone_longitude = dronePos['longitude']
+        drone_altitude = dronePos['altitude']
 
-#         print(drone_latitude, drone_longitude, drone_altitude)
+        print(drone_latitude, drone_longitude, drone_altitude)
 
-#         destination_latitude = float(request.json['latitude'].replace(',', '.'))
-#         destination_longitude = float(request.json['longitude'].replace(',', '.'))
-#         destination_altitude = float(request.json['altitude'].replace(',', '.'))
+        destination_latitude = float(request.json['latitude'].replace(',', '.'))
+        destination_longitude = float(request.json['longitude'].replace(',', '.'))
+        destination_altitude = float(request.json['altitude'].replace(',', '.'))
 
-#         startPos = (drone_latitude, drone_longitude)
-#         endPos = (destination_latitude, destination_longitude)
+        startPos = (drone_latitude, drone_longitude)
+        endPos = (destination_latitude, destination_longitude)
 
-#         print(type(destination_latitude))
-#         print(type(drone_latitude))
+        print(type(destination_latitude))
+        print(type(drone_latitude))
 
-#         if (haversine(startPos, endPos, unit=Unit.METERS) < maxDistanceMoved):
-#             print(haversine(startPos, endPos, unit=Unit.METERS))
-#             #controller.moveto(drone, drone_latitude, drone_longitude, altitude, 0, 0)
-#             #controller.moveto(drone, latitude, longitude, drone_altitude, 0, 0)
-#             return jsonify({
-#                 'message2': f'Position before flight: {drone_latitude}, long: {drone_longitude}, alt: {drone_altitude}',
-#                 'message': f'Flying to lat: {destination_latitude}, long: {destination_longitude}, alt: {destination_altitude}'
-#             }), 200
-#         else:
-#             return jsonify({
-#                 'message': f'Choose a shorter distance.'
-#             })
-#     except:
-#         drone(Landing()).wait()
-#         return jsonify({
-#             'message': 'Failed when trying to fly to GPS coordinates. Landing...',
-#             'error': sys.exc_info()[0]
-#         }), 500
-
-
-# @app.route('/moveBy', methods=['POST'])
-# def move_by():
-#     try:
-#         # Needs translation because of different labels between drone api and unity 
-#         x_movement = request.json['z_movement']
-#         y_movement = request.json['x_movement']
-#         z_movement = -request.json['y_movement']
-#         rotation = request.json['rotation']
-
-#         controller.moveby(drone, x_movement, y_movement, z_movement, rotation)
-#         return jsonify({
-#             'message': f'Moving x dir: {x_movement}, y dir: {y_movement}, z dir: {z_movement}, rotation: {rotation}'
-#         }), 200
-#     except:
-#         drone(Landing()).wait()
-#         return jsonify({
-#             'message': 'Failed when trying to fly to move drone. Landing...',
-#             'error': sys.exc_info()[0]
-#         }), 500
-    
-
-# @app.route('/flyBackward', methods=['POST'])
-# def flyBackwards():
-#     controller.moveby(drone,-1,0,0,0)
-#     return 'flying 1 meter backwards'
+        if (haversine(startPos, endPos, unit=Unit.METERS) < maxDistanceMoved):
+            print(haversine(startPos, endPos, unit=Unit.METERS))
+            #controller.moveto(drone, drone_latitude, drone_longitude, altitude, 0, 0)
+            #controller.moveto(drone, latitude, longitude, drone_altitude, 0, 0)
+            return jsonify({
+                'message2': f'Position before flight: {drone_latitude}, long: {drone_longitude}, alt: {drone_altitude}',
+                'message': f'Flying to lat: {destination_latitude}, long: {destination_longitude}, alt: {destination_altitude}'
+            }), 200
+        else:
+            return jsonify({
+                'message': f'Choose a shorter distance.'
+            })
+    except:
+        drone(Landing()).wait()
+        return jsonify({
+            'message': 'Failed when trying to fly to GPS coordinates. Landing...',
+            'error': sys.exc_info()[0]
+        }), 500
 
 
-# @app.route('/flyForward', methods=['POST'])
-# def flyForwards():
-#     controller.moveby(drone,1,0,0,0)
-#     return 'flying 1 meter forwards'
+@app.route('/moveBy', methods=['POST'])
+def move_by():
+    try:
+        # Needs translation because of different labels between drone api and unity 
+        x_movement = request.json['z_movement']
+        y_movement = request.json['x_movement']
+        z_movement = -request.json['y_movement']
+        rotation = request.json['rotation']
 
-
-# @app.route('/land', methods=['POST'])
-# def land():
-#     try:
-#         controller.land(drone)
-#         return jsonify({
-#             'message': 'Landing drone.'
-#         }), 200
-#     except:
-#         return jsonify({
-#             'message': 'Failed when trying to land drone.',
-#             'error': sys.exc_info()[0]
-#         }), 500
+        controller.moveby(drone, x_movement, y_movement, z_movement, rotation)
+        return jsonify({
+            'message': f'Moving x dir: {x_movement}, y dir: {y_movement}, z dir: {z_movement}, rotation: {rotation}'
+        }), 200
+    except:
+        drone(Landing()).wait()
+        return jsonify({
+            'message': 'Failed when trying to fly to move drone. Landing...',
+            'error': sys.exc_info()[0]
+        }), 500
     
 
-# @app.route('/disconnect', methods=['POST'])
-# def disconnect():
-#     try:
-#         controller.disconnect(drone)
-#         return jsonify({
-#             'message': 'Disconnected from drone.'
-#         }), 200
-#     except:
-#         return jsonify({
-#             'message': 'Failed when trying to disconnect from drone.',
-#             'error': sys.exc_info()[0]
-#         }), 500
+@app.route('/flyBackward', methods=['POST'])
+def flyBackwards():
+    controller.moveby(drone,-1,0,0,0)
+    return 'flying 1 meter backwards'
+
+
+@app.route('/flyForward', methods=['POST'])
+def flyForwards():
+    controller.moveby(drone,1,0,0,0)
+    return 'flying 1 meter forwards'
+
+
+@app.route('/land', methods=['POST'])
+def land():
+    try:
+        controller.land(drone)
+        return jsonify({
+            'message': 'Landing drone.'
+        }), 200
+    except:
+        return jsonify({
+            'message': 'Failed when trying to land drone.',
+            'error': sys.exc_info()[0]
+        }), 500
+    
+
+@app.route('/disconnect', methods=['POST'])
+def disconnect():
+    try:
+        controller.disconnect(drone)
+        return jsonify({
+            'message': 'Disconnected from drone.'
+        }), 200
+    except:
+        return jsonify({
+            'message': 'Failed when trying to disconnect from drone.',
+            'error': sys.exc_info()[0]
+        }), 500
     
