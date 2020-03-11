57441f656eb4c1c5c9b3c1c5261ef53f6f0b23dc
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2020-03-11 15:01:25 +0100

    test

--- camera.py
+++ camera.py
@@ -5,7 +5,7 @@ from base_camera import BaseCamera
 class Camera(BaseCamera):
     """An emulated camera implementation that streams a repeated sequence of
     files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
-    imgs = open('./images/test_image.jpg', 'rb').read()
+    imgs = open('./image/test_image.jpg', 'rb').read()
 
     @staticmethod
     def frames():
