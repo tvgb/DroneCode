09a1ce7cc682f0ac7193316535b2902d2e4e3aab
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2020-03-18 14:05:14 +0100

    test

--- rest_controller.py
+++ rest_controller.py
@@ -30,6 +30,7 @@ def index():
 def gen(camera):
     while True:
         frame = camera.get_frame()
+        print(frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
