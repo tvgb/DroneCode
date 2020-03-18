a1f6a73bc0ea82ce5626ebe592a2d603e7e47c6e
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2020-03-18 14:06:43 +0100

    Test

--- rest_controller.py
+++ rest_controller.py
@@ -30,7 +30,10 @@ def index():
 def gen(camera):
     while True:
         frame = camera.get_frame()
-        print(frame)
+        
+        if frame == None:
+            frame = open('./static/images/mario/mario1.jpg', 'rb').read()
+
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
