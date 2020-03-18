0e43a83163da0844373306d4dc2fb0ac1da7d688
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2020-03-18 20:56:11 +0100

    test

--- thread_camera.py
+++ thread_camera.py
@@ -5,21 +5,25 @@ class ThreadCamera():
     def __init__(self):
         self.frame = None
         self.thread = None
+        self.run = True
 
 
     def start_camera_thread(self, stream):
         thread = threading.Thread(target=self.camera_thread, args=(stream,), daemon=True)
         self.thread = thread
+        self.run = True
+
         thread.start()
 
 
     def stop_camera_thread(self):
+        self.run = False
         self.thread.join()
 
 
     def camera_thread(self, stream):
         
-        while True:
+        while self.run:
             if stream != None:
                 self.frame = stream.frame
 
