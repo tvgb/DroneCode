35b2c6c84f2201526e683b72d9a2ae223e385513
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2020-03-18 21:39:13 +0100

    Cleanup

--- controller.py
+++ controller.py
@@ -12,9 +12,11 @@ def get_drone(ip):
     drone = olympe.Drone(ip, loglevel=1) # loglevel 1 is only errors
     return drone
 
+
 def position(drone):
     drone(GPSFixStateChanged(_policy = 'wait'))
     return drone.get_state(HomeChanged)
+
 
 def takeoff(drone):
     drone(TakeOff()).wait()
@@ -27,6 +29,7 @@ def moveby(drone,x,y,z,psi=0.0):
 def land(drone):
     drone(Landing()).wait()
 
+
 def moveto(drone, latitude, longitude, altitude, orientation_mode, heading):
     drone(moveTo(
         latitude,
@@ -36,8 +39,10 @@ def moveto(drone, latitude, longitude, altitude, orientation_mode, heading):
         heading
     )).wait()
 
+
 def disconnect(drone):
     drone.disconnection()
+
 
 def set_gimbal(drone,set_mode,set_yaw,set_pitch,set_roll):
     drone(gimbal.set_target(
@@ -55,46 +60,3 @@ def set_gimbal(drone,set_mode,set_yaw,set_pitch,set_roll):
 def start_stream(drone):
     stream = Stream(drone)
 
-    
-
-
-'''
-
-print('Setting gimbal')
-set_gimbal(drone, "position", 0.0, 0.0, 0.0)
-print('Gimbal set')
-
-time.sleep(10)
-
-print('Setting gimbal')
-set_gimbal(drone, "position", 1.0, 0.0, 0.0)
-print('Gimbal set')
-
-time.sleep(10)
-
-print('Setting gimbal')
-set_gimbal(drone, "position", 0.0, 0.0, 0.0)
-print('Gimbal set')
-
-
-
-drone.connection()
-drone(TakeOff()).wait()
-drone(Landing()).wait()
-drone.disconnection()
-
-
-
-def camera_straight(drone):
-    drone(gimbal.set_target(
-        gimbal_id=0,
-        control_mode="position",
-        yaw_frame_of_reference="none",
-        yaw=0.0,
-        pitch_frame_of_reference="absolute",
-        pitch=0.0,
-        roll_frame_of_reference="none",
-        roll=0.0,
-    )).wait() 
-
-'''
